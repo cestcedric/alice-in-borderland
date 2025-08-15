bb63f1712c91b36fa3caa1f21e829a9dd970efef
Cedric Stark <cedric.stark@outlook.com>
2025-08-15 02:53:04 +0200

    Init: fetch all pages and create 1 PDF per chapter

---
+++ alice_in_borderland.py
@@ -0,0 +1,80 @@
+import os
+import requests
+from bs4 import BeautifulSoup
+from io import BytesIO
+from PIL import Image
+
+
+START_URL = "https://alice-in-borderland.com/manga/alice-in-borderland-chapter-1/"
+OUTPUT_PATH = os.path.join(os.getcwd(), "pdf")
+COMPRESSION = 100
+
+
+def handle_chapter(url: str) -> str | None:
+    title, filename = get_chapter_name(url)
+    print(title)
+
+    # All pages as separate images
+    chapter = requests.get(url)
+    soup = BeautifulSoup(chapter.content, "html.parser")
+    content = soup.find("div", class_="entry-content").find("p")
+
+    pages = get_pages(content)
+    create_pdf(pages, title, filename)
+
+    return get_next_chapter_url(soup)
+
+
+def get_chapter_name(url: str) -> tuple[str]:
+    parts = url.split("/")[-2].split("-")
+    title = " ".join(word[0].upper() + word[1:] for word in parts)
+    filename = "_".join(parts) + ".pdf"
+    return title, filename
+
+
+def get_pages(soup: BeautifulSoup) -> list:
+    pages = []
+    page_links = [img["src"] for img in soup.find_all("img")]
+
+    for i, url in enumerate(page_links):
+        response = requests.get(url)
+        img = Image.open(BytesIO(response.content))
+        if img.mode != "RGB":
+            img = img.convert("RGB")
+
+        if COMPRESSION < 100:
+            buffer = BytesIO()
+            img.save(buffer, format="JPEG", quality=COMPRESSION)
+            buffer.seek(0)
+            img = Image.open(buffer)
+
+        pages.append(img)
+        print(f"Page {i + 1} / {len(page_links)}")
+
+    return pages
+
+
+def get_next_chapter_url(soup: BeautifulSoup) -> str | None:
+    nav_next = soup.find("div", class_="nav-next")
+
+    if nav_next is None:
+        return None
+
+    return nav_next.find("a")["href"]
+
+
+def create_pdf(pages: list, title: str, filename: str) -> None:
+    output_path = os.path.join(OUTPUT_PATH, filename)
+    if os.path.isfile(output_path):
+        print(f"{title} already exists!")
+    else:
+        pages[0].save(output_path, save_all=True, append_images=pages[1:])
+
+
+if __name__ == "__main__":
+    url = START_URL
+
+    while url is not None:
+        url = handle_chapter(url)
+
+    print("Done!")
